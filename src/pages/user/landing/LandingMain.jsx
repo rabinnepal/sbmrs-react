bade6ed3c330fdfa8a5d1c3c99420a2c06ccf67c
Rabin Nepal <rabeen1008@gmail.com>
2023-04-18 23:58:56 +0545

    landing ui

--- src/pages/user/landing/LandingMain.jsx
+++ src/pages/user/landing/LandingMain.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import NowPlaying from "./NowPlaying";
+import ComingSoon from "./ComingSoon";
+import TopMovies from "./TopMovies";
+import { Box, Container, InputAdornment, TextField } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 const LandingMain = () => {
   return (
     <div>
+      <Container sx={{ textAlign: "center", my: 2 }}>
+        <TextField
+          placeholder="Search"
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Container>
       <NowPlaying />
+      <ComingSoon />
+      <TopMovies />
     </div>
   );
 };
